a626a9c0541517086ac1b79b9031c561c1a4f1a1
Adam Palmer <theadampalmer@gmail.com>
2020-07-16 17:39:10 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(person_name)
+    @human_name = person_name
+  end
+
+  def name
+    @human_name
+  end
+
+  def job=(their_job)
+    @work = their_job
+  end
+
+  def job
+    @work
+  end
+end
