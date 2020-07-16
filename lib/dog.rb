905abc43becab4b8a21479846363b9a661c009c6
Adam Palmer <theadampalmer@gmail.com>
2020-07-16 17:31:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,4 @@
 class Dog
- 
   def initialize(name)
     @name = name
   end
@@ -7,9 +6,7 @@ class Dog
   def name
     @name
   end
- 
-  def name=(new_name)
-    @name = new_name
-  end
- 
 end
+
+    fido = Dog.new
+    fido.name = "Fido"
